d14d63250004d0818fb0b89e97c53722e8b6dfd3
Robinhood75t <robinparjapatikumar@gmail.com>
2026-04-10 12:51:34 +0530

    validAnagram

---
+++ Array & Hashing/validAnagram.cpp
@@ -0,0 +1,33 @@
+// Brute Forece approach : O(nlogn) time complexity & O(1) space complexity
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s == t;
+    }
+};
+
+
+// Optimal approach : O(n) time complexity & O(n) in general , but O(1) space complexity
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        string lar = (s.length() >= t.length()) ? s : t;
+        string sml = (s.length() >= t.length()) ? t : s;
+        unordered_map<char,int> mymap;
+        for(int i = 0; i < lar.length(); i++){
+            mymap[lar[i]]++;
+        }
+        for(int i = 0; i < sml.length(); i++){
+            if(mymap.find(sml[i]) != mymap.end()){
+                mymap[sml[i]]--;
+                if(mymap[sml[i]] == 0){
+                    mymap.erase(sml[i]);
+                }
+            }
+        }
+        return mymap.empty();
+    }
+};
+
