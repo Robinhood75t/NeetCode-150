32d7a144c09bfb41aaeef9c97d0d1efa3e8e995d
Robinhood75t <robinparjapatikumar@gmail.com>
2026-04-14 11:56:37 +0530

    ValidPalindrome

---
+++ Two Pointers/validPalindrome.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str = "";
+        for(char& ch: s){
+            if(ch != ' ' && isalnum(ch)){
+                str += tolower(ch);
+            }
+        }
+        string t = str;
+        reverse(t.begin(),t.end());
+        return t == str;
+    }
+};
