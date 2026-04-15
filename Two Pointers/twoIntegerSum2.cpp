35e17ff03891bc28a51bb1092f6e5b6721bf479e
Robinhood75t <robinparjapatikumar@gmail.com>
2026-04-15 11:54:24 +0530

    Opt-TwoIntergerSum2

--- Two Pointers/twoIntegerSum2.cpp
+++ Two Pointers/twoIntegerSum2.cpp
@@ -1,3 +1,5 @@
+
+// Brute Forece Approach Time Complexity O(n^2) space complexity O(1)
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
@@ -13,3 +15,28 @@ public:
         return {};
     }
 };
+
+Two pointer approach --
+
+// Optimal Approach Time complexity O(n) space complexity O(1)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ans(2);
+        int n = numbers.size();
+        int i = 0,j = n - 1;
+        while(i < j){
+            if(numbers[i] + numbers[j] == target){
+                return {i + 1 , j + 1};
+            }
+            else if(numbers[i] + numbers[j] > target){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return {};
+    }
+};
+
