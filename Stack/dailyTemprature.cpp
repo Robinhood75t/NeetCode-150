397449273c253f38cfe02daf028773664166001b
Robinhood75t <robinparjapatikumar@gmail.com>
2026-04-17 11:34:10 +0530

    DailyTemperature

---
+++ Stack/dailyTemprature.cpp
@@ -0,0 +1,17 @@
+// Brute force Time complexity O(n^2) space complexity O(n)
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> ans(n,0);
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                if(temperatures[i] < temperatures[j]){
+                    ans[i] = j - i;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
