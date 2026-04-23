70d7a6cb8a36b7b7621a8ac9ae4992173e659732
Robinhood75t <robinparjapatikumar@gmail.com>
2026-04-23 13:54:15 +0530

    opt-FindMinInRotatedSortedArr

--- Binary Search/FindMinimumInRotatedSortedArray.cpp
+++ Binary Search/FindMinimumInRotatedSortedArray.cpp
@@ -19,3 +19,28 @@ public:
         return minVal;
     }
 };
+// Binary search solution - O(log n)
+
+class Solution {
+public:
+    int findMin(vector<int> &nums) {
+        int n = nums.size();
+        int st = 0, end = n - 1;
+        int minVal = INT_MAX;
+        while(st <= end){
+            if(nums[st] < nums[end]){
+                minVal = min(nums[st], minVal);
+                break;
+            }
+            int mid = st + (end - st) / 2;
+            minVal = min(minVal,nums[mid]);
+
+            if(nums[mid] >= nums[st]){
+                st = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return minVal;
+    }
+};
