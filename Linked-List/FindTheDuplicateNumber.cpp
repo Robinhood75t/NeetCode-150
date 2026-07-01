2cc4aad91ead95982be6dbc11c9b91687590f916
Robinhood75t <robinparjapatikumar@gmail.com>
2026-07-01 16:24:34 +0530

    FindtheDuplicateNumber

---
+++ Linked-List/FindTheDuplicateNumber.cpp
@@ -0,0 +1,16 @@
+// baccho vala solution.
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_set<int> myset;
+        for(int i = 0; i < nums.size(); i++){
+            if(myset.find(nums[i]) != myset.end()){
+                return nums[i];
+            }else{
+                myset.insert(nums[i]);
+            }
+        }
+        return -1;
+    }
+};
+ 
